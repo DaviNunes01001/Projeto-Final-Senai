f887f4f2b42b9937eb983d212b81187ca03ccc39
hakizao6074 <davizinhodjar@gmail.com>
2026-04-28 10:44:36 -0300

    fit: criação do header e alocação das rotas no app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,8 @@ function Header() {
   return (
     <>
       <div className={styles.header}>
+        <h1>Gerenciador de Produtos</h1>
+        <p>Uma produção dos Manos Inc</p>
         <nav className={styles.nav}>
             <Link to="/">Home</Link>
             <Link to="/Contato">Contato</Link>
