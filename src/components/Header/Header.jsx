e27e99981aec9eca604c683df5d0695b27041d55
hakizao6074 <davizinhodjar@gmail.com>
2026-04-28 08:50:09 -0300

    fit: criação de base de components e puxando css global de index.css par aqruivos de componentes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import  styles from './Header.module.css'
+
+function Header() {
+    return (
+        <>
+        <div className={styles.header}>
+            <nav className={styles.nav}></nav>
+        </div>
+        </>
+    )
+}
+
+export default Header
