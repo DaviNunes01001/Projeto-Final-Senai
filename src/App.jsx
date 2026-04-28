43fff1cbbd73ceb65e000acbec4b7f3cd679d9d6
hakizao6074 <davizinhodjar@gmail.com>
2026-04-28 11:27:25 -0300

    fit: alterações de arquivos inuteis e corrigindo erros

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "../public/Home/Home"
 import Contato from "../public/Contato/Contato"
-import Produto from "../public/Produto/index"
+import Produto from "../public/Produto/Produto"
 import Sobre from "../public/Sobre/Sobre"
 
 function App() {
