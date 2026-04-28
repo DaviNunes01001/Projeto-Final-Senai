8e839dfd9ab65e3f0561d5ff2ec055ec6850b066
hakizao6074 <davizinhodjar@gmail.com>
2026-04-28 10:20:38 -0300

    push

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import  styles from './Header.module.css'
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 function Header() {
-    return (
-        <>
-        <div className={styles.header}>
-            <nav className={styles.nav}></nav>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className={styles.header}>
+        <nav className={styles.nav}>
+            <Link to="/">Home</Link>
+            <Link to="/Contato">Contato</Link>
+            <Link to="/Produto">Produto</Link>
+        </nav>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
